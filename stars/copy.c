0773bc2bc24b1a6f9c885494048a51d84a27728b
Valentina Goncharenko <goncharenko.vp@ispras.ru>
2023-10-02 12:09:39 +0300

    fixed copy and nested stars

--- stars/copy.c
+++ stars/copy.c
@@ -2,26 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int for_copyin = 8;
+int for_copyin1 = 8;
 int for_copyprivate = 0;
+int for_copyin2 = 8;
 
 int main() {
     
     omp_set_num_threads(4);
 
+    #pragma omp threadprivate(for_copyin1, for_copyin2)
+
+    printf("Before parallel region, for_copyin1 = %d\n", for_copyin1);
+    printf("Before parallel region, for_copyin2 = %d\n", for_copyin2);
+
+    for_copyin1 = 88;
+    for_copyin2 = 88;
+    printf("Values of variables has changed to 88\n\n");
+
     // 2 The copyin clause is used to initialize threadprivate data upon entry to a parallel region. The
     // 3 value of the threadprivate variable in the primary thread is copied to the threadprivate variable of
     // 4 each other team member.
-    #pragma omp threadprivate(for_copyin)
-    #pragma omp parallel copyin(for_copyin) 
+    printf("copyin(for_copyin1)\n");
+    #pragma omp parallel copyin(for_copyin1)
     {
-        for_copyin += omp_get_thread_num();
-        printf("Thread %d: for_copyin = %d\n", omp_get_thread_num(), for_copyin);
-
+        printf("Thread %d: for_copyin1 = %d\n", omp_get_thread_num(), for_copyin1);
+        printf("Thread %d: for_copyin2 = %d\n", omp_get_thread_num(), for_copyin2);
         #pragma omp barrier
     }
 
-    printf("for_copyin from master thread = %d\n\n", for_copyin);
+    printf("\n");
 
     #pragma omp threadprivate(for_copyprivate)
     #pragma omp parallel 
--- stars/nested.c
+++ stars/nested.c
@@ -19,48 +19,51 @@ int main(int argc, char *argv[]) {
         #pragma omp atomic
         total_prints[omp_get_level() - 1]++;
 
-        printf("I am a thread number %d among %d threads on my level %d with team size %d\n", 
+        printf("I am a thread number %d among %d threads on my level %d with team size %d and ansector %d\n", 
             omp_get_thread_num(),
             omp_get_num_threads(),
             omp_get_level(),
-            omp_get_team_size(omp_get_level()));
+            omp_get_team_size(omp_get_level()),
+            omp_get_ancestor_thread_num(omp_get_level() - 1));
 
         #pragma omp barrier
-        #pragma omp critical
-        #pragma omp master
-        printf("Current prints in level %d: %d\n", omp_get_level(), total_prints[omp_get_level() - 1]);
+        //#pragma omp critical
+        //#pragma omp master
+        //printf("Current prints in level %d: %d\n", omp_get_level(), total_prints[omp_get_level() - 1]);
 
         #pragma omp parallel num_threads(2)
         {
             #pragma omp atomic
             total_prints[omp_get_level() - 1]++;
 
-            printf("I am a thread number %d among %d threads on my level %d with team size %d\n", 
+            printf("I am a thread number %d among %d threads on my level %d with team size %d and ansector %d\n", 
                 omp_get_thread_num(),
                 omp_get_num_threads(),
                 omp_get_level(),
-                omp_get_team_size(omp_get_level()));
+                omp_get_team_size(omp_get_level()),
+                omp_get_ancestor_thread_num(omp_get_level() - 1));
 
             #pragma omp barrier
-            #pragma omp critical
-            #pragma omp master
-            printf("Current prints in level %d: %d\n", omp_get_level(), total_prints[omp_get_level() - 1]);
+            //#pragma omp critical
+            //#pragma omp master
+            //printf("Current prints in level %d: %d\n", omp_get_level(), total_prints[omp_get_level() - 1]);
 
             #pragma omp parallel num_threads(2)
             {
                 #pragma omp atomic
                 total_prints[omp_get_level() - 1]++;
 
-                printf("I am a thread number %d among %d threads on my level %d with team size %d\n", 
+                printf("I am a thread number %d among %d threads on my level %d with team size %d and ansector %d\n", 
                     omp_get_thread_num(),
                     omp_get_num_threads(),
                     omp_get_level(),
-                    omp_get_team_size(omp_get_level()));
+                    omp_get_team_size(omp_get_level()),
+                    omp_get_ancestor_thread_num(omp_get_level() - 1));
 
                 #pragma omp barrier
-                #pragma omp critical
-                #pragma omp master
-                printf("Current prints in level %d: %d\n", omp_get_level(), total_prints[omp_get_level() - 1]);
+                //#pragma omp critical
+                //#pragma omp master
+                //printf("Current prints in level %d: %d\n", omp_get_level(), total_prints[omp_get_level() - 1]);
             }
         }
     }
